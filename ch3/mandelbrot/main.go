d89b774d48440b8476e6065afca521cdb7a2bae4
Nicolas Montanaro <nsmon@rbox.co>
2018-02-28 11:35:59 -0500

    gofmt exercise 3.6, make alpha 255 always

--- ch3/mandelbrot/main.go
+++ ch3/mandelbrot/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-var palette = [5]color.RGBA {
+var palette = [5]color.RGBA{
 	color.RGBA{0, 100, 100, 255},
 	color.RGBA{32, 107, 20, 255},
 	color.RGBA{237, 255, 255, 255},
@@ -19,14 +19,14 @@ var palette = [5]color.RGBA {
 func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height = 1024, 1024
+		width, height          = 1024, 1024
 	)
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py) / height * (ymax - ymin) + ymin
+		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x := float64(px) / width * (xmax - xmin) + xmin
+			x := float64(px)/width*(xmax-xmin) + xmin
 
 			subPixels := []color.RGBA{}
 			for i := 0; i < 2; i++ {
@@ -51,10 +51,10 @@ func mandelbrot(z complex128) color.RGBA {
 	const contrast = 15
 
 	var v complex128
-	for n:= uint8(0); n < iterations; n++ {
-		v = v * v + z
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return palette[n % 5]
+			return palette[n%5]
 		}
 	}
 
@@ -62,13 +62,12 @@ func mandelbrot(z complex128) color.RGBA {
 }
 
 func getAverage(colors []color.RGBA) color.RGBA {
-	var r, g, b, a uint8
+	var r, g, b uint8
 	for _, color := range colors {
 		r += color.R
 		g += color.G
 		b += color.B
-		a += color.A
 	}
 
-	return color.RGBA{r / 4, g / 4, b / 4, a / 4}
+	return color.RGBA{r / 4, g / 4, b / 4, 255}
 }
